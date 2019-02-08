c4e3c498c522c677542b1427a4d82926f2780118
chriskes <47326672+chriskes@users.noreply.github.com>
2019-02-08 12:56:44 +0100

    Create 1.3_chessboard.js

---
+++ 1.3_chessboard.js
@@ -0,0 +1,12 @@
+var size = 8;
+var x = '# # # #'
+var y = ' # # # #'
+
+for (let lines = 0; lines <= size; lines += 1) {
+ if (lines % 2 == 0) {
+     console.log (x)
+ }
+    else {
+        console.log(y)
+    }
+}
